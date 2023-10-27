c90868cf56e04fec51443578ebcaa4a9a0da719a
andrewcheung27 <andrewcheung27@gmail.com>
2023-10-26 21:30:19 -0700

    trying google maps, map not showing

---
+++ src/app/map.js
@@ -0,0 +1,52 @@
+'use client'
+
+import Image from 'next/image'
+import { useState, useCallback } from 'react';
+import { GoogleMap, useJsApiLoader } from '@react-google-maps/api';
+
+const containerStyle = {
+  width: '400px',
+  height: '400px'
+};
+
+const center = {
+  lat: -3.745,
+  lng: -38.523
+};
+
+export default function Map() {
+  debugger;
+  const { isLoaded } = useJsApiLoader({
+    id: 'google-map-script',
+    googleMapsApiKey: process.env.MAPS_KEY
+  })
+debugger;
+  const [map, setMap] = useState(null)
+
+  const onLoad = useCallback(function callback(map) {
+    debugger;
+    // This is just an example of getting and using the map instance!!! don't just blindly copy!
+    const bounds = new window.google.maps.LatLngBounds(center);
+    map.fitBounds(bounds);
+
+    setMap(map)
+  }, [])
+
+  const onUnmount = useCallback(function callback(map) {
+    debugger;
+    setMap(null)
+  }, [])
+
+  return isLoaded ? (
+      <GoogleMap
+        mapContainerStyle={containerStyle}
+        center={center}
+        zoom={10}
+        onLoad={onLoad}
+        onUnmount={onUnmount}
+      >
+        { /* Child components, such as markers, info windows, etc. */ }
+        <></>
+      </GoogleMap>
+  ) : <></>
+}
--- src/app/page.js
+++ src/app/page.js
@@ -1,45 +1,12 @@
-'use client'
-
 import Image from 'next/image'
-import { Wrapper, Status } from '@googlemaps/react-wrapper'
-import { useRef, useEffect } from 'react';
-
-
-const MyApp = () => (
-  <Wrapper apiKey={process.env.MAPS_KEY}>
-    <MyMapComponent />
-  </Wrapper>
-);
+import Map from './map'
 
 
 export default function Home() {
-  const ref = useRef();
-
-  useEffect(() => {
-    debugger;
-    new window.google.maps.Map(ref.current, {
-      center: {lat: 30, lng: 30},
-      zoom: 100,
-    });
-    debugger;
-  });
-
   return (
     <>
       <h1>Welcome to CSC 307</h1>
-      <Wrapper apiKey={process.env.MAPS_KEY}>
-        <div ref={ref} id="map">aaaaaaaa</div>
-      </Wrapper>
-      <p>
-        This application is a Next.js application. It already contains a way to login and sign-up as well as a rough ToDo list application as a way to demonstrate create and update of a todo. 
-      </p>
-      <h2>Documentation</h2>
-      <ul>
-        <li>NextJS: <a href="https://nextjs.org/docs">https://nextjs.org/docs</a></li>
-        <li>Material UI: <a href="https://mui.com/material-ui/getting-started/">https://mui.com/material-ui/getting-started/</a></li>
-        <li>Prisma: <a href="https://www.prisma.io/docs/getting-started">https://www.prisma.io/docs/getting-started</a></li>
-      </ul>
-      <h2></h2>
+      <Map></Map>
     </>
   )
 }
