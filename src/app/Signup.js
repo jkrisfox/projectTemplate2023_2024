74cbb64be47594a560ec1241133feba40f8cd58d
wesleytam88 <wesleytam88@gmail.com>
2023-10-26 20:15:39 -0700

    Change Sign Up button to white

--- src/app/Signup.js
+++ src/app/Signup.js
@@ -1,12 +1,12 @@
 'use client'
 
-import { useState } from 'react';
 import Button from '@mui/material/Button';
 
 export default function Signup() {
   return (
     <Button variant="outlined" 
             color="inherit" 
-            href="/Signup">Signup</Button>
+            href="/Signup"
+            sx={{color:'white'}}>Signup</Button>
   );
 }
