1a41ac8e994566f0946383ee9bcbdc26f2b852bb
andrewcheung27 <andrewcheung27@gmail.com>
2023-10-26 20:42:58 -0700

    mmmmmm

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,35 @@
+'use client'
+
 import Image from 'next/image'
-import mapsKey from 'src/lib/mapsAPIKey.js'
+import { Wrapper, Status } from '@googlemaps/react-wrapper'
+import { useRef, useEffect } from 'react';
+
+
+const MyApp = () => (
+  <Wrapper apiKey={process.env.MAPS_KEY}>
+    <MyMapComponent />
+  </Wrapper>
+);
+
 
 export default function Home() {
+  const ref = useRef();
+
+  useEffect(() => {
+    debugger;
+    new window.google.maps.Map(ref.current, {
+      center: {lat: 30, lng: 30},
+      zoom: 100,
+    });
+    debugger;
+  });
+
   return (
     <>
       <h1>Welcome to CSC 307</h1>
+      <Wrapper apiKey={process.env.MAPS_KEY}>
+        <div ref={ref} id="map">aaaaaaaa</div>
+      </Wrapper>
       <p>
         This application is a Next.js application. It already contains a way to login and sign-up as well as a rough ToDo list application as a way to demonstrate create and update of a todo. 
       </p>
