24956fadfa8a9b1a1f0675596d535b806c0083fd
Caden Tierney <ctierney@calpoly.edu>
2023-11-02 16:16:58 -0700

    Validate user input from setup page

--- src/app/api/users/[id]/route.js
+++ src/app/api/users/[id]/route.js
@@ -17,6 +17,34 @@ export async function PUT(request, { params }) {
   }
 
   const { name, phoneNumber, location } = await request.json();
+  
+  // Validate name
+  if (!name) {
+    return NextResponse.json({error: 'the name field must be supplied'}, {status: 400});
+  }
+
+  if (name.length > 64) {
+    return NextResponse.json({error: 'name is too long'}, {status: 400});
+  }
+
+  if (phoneNumber) {
+    // Validate phone number
+    const numberIsValid = phoneNumber.match(/^(\+\d{1,2}\s?)?1?\-?\.?\s?\(?\d{3}\)?[\s.-]?\d{3}[\s.-]?\d{4}$/)
+    if (!numberIsValid) {
+      return NextResponse.json({error: 'phone number is incorrectly formatted'}, {status: 400});
+    }
+
+    if (phoneNumber.length > 16) {
+      return NextResponse.json({error: 'phone number is too long'}, {status: 400});
+    }
+  }
+
+  // Validate location
+  if (location && (location.length > 64)) {
+    return NextResponse.json({error: 'location is too long'}, {status: 400});
+  }
+
+
   try {
     const user = await prisma.user.update({
       where: {
