fefba2dd5698c5b713bcae21048d6634fda31ca1
Joseph Dewar <superjoeyd123@gmail.com>
2023-11-02 20:20:12 -0700

    Fixed local env

--- src/app/page.js
+++ src/app/page.js
@@ -5,7 +5,6 @@ import Map from './map'
 export default function Home() {
   return (
     <>
-      <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&callback=initMap" async defer></script>
       <h1>Welcome to CSC 307</h1>
       <div>
         <Map></Map> WHAT IS HAPPENING??????
