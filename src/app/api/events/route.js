8d3099de0c6ff8ca9e570ddde9a50e3bb996afa1
Zhi He <zhihe122@gmail.com>
2023-11-09 22:51:54 -0800

    mostly completed events api, now has GET, POST, DELETE, PATCH (CRUD)

--- src/app/api/events/route.js
+++ src/app/api/events/route.js
@@ -2,7 +2,6 @@ import { NextResponse } from "next/server";
 import prisma from "@/lib/db";
 import { checkLoggedIn } from "@/lib/auth";
 import { USER_NOT_SIGNED_IN } from "@/lib/response";
-import { useSearchParams } from "next/navigation";
 
 export async function POST(request) {
   // const loggedInData = await checkLoggedIn();
@@ -96,21 +95,79 @@ export async function GET(request) {
   return USER_NOT_SIGNED_IN;
 }
 
-// export async function DELETE() {
-//   const responseData = await request.json();
-//   const { id } = responseData;
-//     let eventDeleted;
-//     try {
-//       user = await prisma.user.delete({
-//         where: {
-//           id
-//         }
-//       });
-//     } catch (e) {
-//       console.log(e.message);
-//       return NextResponse.json({error: e.message}, {status: 500 })
-//     }
-//     return NextResponse.json(user);
-//   }
-//   return USER_NOT_SIGNED_IN;
-// }
+export async function DELETE(request) {
+  // delete events and cascade delete all the event attendees
+  const responseData = await request.json();
+  if (true) {
+    const { id } = responseData;
+    let eventDeleted;
+    try {
+      eventDeleted = await prisma.Event.delete({
+        where: {
+          id,
+        },
+      });
+    } catch (e) {
+      console.log(e.message);
+      return NextResponse.json({ error: e.message }, { status: 500 });
+    }
+    return NextResponse.json(eventDeleted);
+  }
+  return USER_NOT_SIGNED_IN;
+}
+
+export async function PATCH(request) {
+  // delete events and cascade delete all the event attendees
+  const responseData = await request.json();
+  if (true) {
+    const {
+      id,
+      eventName,
+      location,
+      startTime,
+      endTime,
+      maxAttendee,
+      filterIds,
+    } = responseData;
+    const eventData = {
+      eventName: eventName,
+      location: location,
+      startTime: startTime,
+      endTime: endTime,
+      maxAttendee: maxAttendee,
+      hostId: 1, // change this to something more dynamic
+      EventFilter: {
+        // add filters to the events and connect them to existing possible filters
+        create: filterIds.map((id) => ({
+          possibleFilter: {
+            connect: {
+              id: id,
+            },
+          },
+        })),
+      },
+    };
+    console.log(eventData);
+    let updatedEvent, deletedFilters;
+    try {
+      deletedFilters = await prisma.EventFilter.deleteMany({
+        where: {
+          eventId: id,
+        },
+      });
+      updatedEvent = await prisma.Event.update({
+        where: {
+          id,
+        },
+        data: eventData,
+      });
+      console.log(deletedFilters);
+      console.log(updatedEvent);
+    } catch (e) {
+      console.log(e.message);
+      return NextResponse.json({ error: e.message }, { status: 500 });
+    }
+    return NextResponse.json(updatedEvent);
+  }
+  return USER_NOT_SIGNED_IN;
+}
