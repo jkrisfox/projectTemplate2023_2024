7ff91aa17128f3e0c1d87a554b66bbb2e41eedbb
andrewcheung27 <andrewcheung27@gmail.com>
2023-11-14 17:14:47 -0800

    did user seeding and put stuff in main()

--- db_seeding/seed.js
+++ db_seeding/seed.js
@@ -1,11 +1,13 @@
+const bcrypt = require('bcryptjs');
 const fs = require('fs');
 const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient();
 
 
-// create records for seasons based on a list of strings
+// creates records for seasons based on a list of strings.
 // each element in the list is comma-separated: name, start, end
 async function insertSeasons(data) {
+    console.log("inserting seasons...");
     let name, start, end;
     for (const line of data) {
         [name, start, end] = line.split(", ");
@@ -20,27 +22,73 @@ async function insertSeasons(data) {
             });
         }
     }
+    console.log("finished inserting seasons\n");
 }
 
-function seedSeasons() {
+async function seedSeasons() {
     const filepath = "db_seeding/seasons.csv";
     var data = fs.readFileSync(filepath, "utf8").split("\n");
-    insertSeasons(data);
+    await insertSeasons(data);
 }
 
 
-// TODO
-// function seedUsers() {
-// TODO: make arrays, put this in loop
-//     await prisma.user.create({
-//         data: {
-//             username: usernames[i], 
-//             email: emails[i], 
-//             password: passwords[i]
-//         }
-//     })
-// }
+// creates records for users based on three arrays of the same length
+async function insertUsers(usernames, emails, passwords) {
+    console.log("inserting users...");
+    for (let i = 0; i < usernames.length; i++) {
+        console.log("inserting", [usernames[i], emails[i], passwords[i]].join(", "));
+        await prisma.user.create({
+            data: {
+                username: usernames[i], 
+                email: emails[i], 
+                // the hash that this project uses
+                password: await bcrypt.hash(passwords[i], 10)
+            }
+        });
+    }
+    console.log("finished inserting users\n");
+}
 
 
-// call all the seeding functions
-seedSeasons();
+async function seedUsers() {
+    let usernames = ["AliceAndVerdict", "BobOrVegana"];
+    let emails = ["alice@gmail.com", "bob@gmail.com"];
+    // plaintext passwords
+    let passwords = ["password", "password"];
+
+    await insertUsers(usernames, emails, passwords);
+}
+
+
+async function main() {
+    let options = {
+        seasons: true, 
+        users: true, 
+        listings: true, 
+        reviews: true, 
+        images: true
+    };
+
+    console.log("sowing seeds...\n");
+
+    if (options.seasons) {
+        await seedSeasons();
+    }
+    if (options.users) {
+        await seedUsers();
+    }
+    // if (options.listings) {
+    //     await seedListings();
+    // }
+    // if (options.reviews) {
+    //     await seedReviews();
+    // }
+    // if (options.images) {
+    //     await seedImages();
+    // }
+
+    console.log("finished seeding database!");
+}
+
+
+main();
