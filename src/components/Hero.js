3d0d04ee8f1d368db8517f5b686898f6313a3d1a
wesleytam88 <wesleytam88@gmail.com>
2023-11-16 18:21:59 -0800

    Add link to about us page

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -105,7 +105,7 @@ function Hero() {
             <Button variant="contained" color="primary" sx={{ color: "white" }}>
               Get started now
             </Button>
-            <Button variant="outlined" color="primary">
+            <Button variant="outlined" color="primary" href="about">
               See benefits
             </Button>
           </Box>
