af79e0aee1502193c34260ea39300e5f6255bf6f
Adrian Nguyen <hngt.adrian@gmail.com>
2023-11-29 15:11:54 -0800

    Add like and post fetch calls

--- src/app/forum/page.js
+++ src/app/forum/page.js
@@ -1,5 +1,5 @@
 'use client'
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import ThumbUpIcon from '@mui/icons-material/ThumbUp';
 import ThumbDownIcon from '@mui/icons-material/ThumbDown';
 import CommentIcon from '@mui/icons-material/Forum';
@@ -7,8 +7,86 @@ import FlagIcon from '@mui/icons-material/Flag';
 import '../globals.css'; 
 
 const Forum = () => {
+  // const samplePosts = [
+  //   {
+  //     title: 'Lost My Gains!',
+  //     author: 'GymNoob',
+  //     tags: ['Fitness', 'Gym'],
+  //     likes: 16,
+  //     dislikes: 2,
+  //     comments: 4,
+  //   },
+  //   {
+  //     title: 'The Great Protein Shake Debate',
+  //     author: 'ShakeLover',
+  //     tags: ['Protein', 'Humor'],
+  //     likes: 32,
+  //     dislikes: 1,
+  //     comments: 7,
+  //   },
+  //   {
+  //     title: 'How to Avoid Eye Contact at the Gym',
+  //     author: 'GymHider',
+  //     tags: ['Gym', 'Etiquette'],
+  //     likes: 19,
+  //     dislikes: 0,
+  //     comments: 3,
+  //   },
+  //   {
+  //     title: 'Cardio vs. Lifting',
+  //     author: 'FitFreak',
+  //     tags: ['Cardio', 'Lifting'],
+  //     likes: 45,
+  //     dislikes: 4,
+  //     comments: 12,
+  //   },
+  //   {
+  //     title: 'Disappearing Gym Socks',
+  //     author: 'SockDetective',
+  //     tags: ['Funny', 'Gym'],
+  //     likes: 22,
+  //     dislikes: 3,
+  //     comments: 5,
+  //   },
+  //   {
+  //     title: 'Muscles: How Many Are Too Many?',
+  //     author: 'MuscleMania',
+  //     tags: ['Muscles', 'Humor'],
+  //     likes: 38,
+  //     dislikes: 2,
+  //     comments: 8,
+  //   },
+  //   {
+  //     title: 'Protein Powder: A Love Story',
+  //     author: 'ProteinAddict',
+  //     tags: ['Protein', 'Love'],
+  //     likes: 33,
+  //     dislikes: 2,
+  //     comments: 7,
+  //   },
+  // ];
 
+  const [postTitle, setPostTitle] = useState('');
+  const [postDescription, setPostDescription] = useState('');
   const [showNewPostPopup, setShowNewPostPopup] = useState(false);
+  const [selectedTags, setSelectedTags] = useState([]);
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      try {
+        const response = await fetch("/api/forums", {
+          method: "GET"
+        });
+        const data = await response.json();
+        console.log("Forum data", data);
+        setPosts(data);
+      } catch (error) {
+        console.error("Failed to fetch [posts]:", error);
+      }
+    };
+    fetchPosts();
+  }, []);
 
   const handleNewPostClick = () => {
     setShowNewPostPopup(true);
@@ -23,14 +101,14 @@ const Forum = () => {
   const handleLikeClick = async (postId) => {
     try {
       // Make a fetch request to your server endpoint
-      const response = await fetch(`/api/like/${postId}`, {
+      const response = await fetch(`/api/votes/${postId}`, {
         method: 'POST',
-        // You may need to include headers and credentials based on your setup
+        body: JSON.stringify({
+          voteType: "UPVOTE"
+        }),
       });
 
       if (response.ok) {
-        // If the request is successful, update the local state or re-fetch posts
-        // For simplicity, let's assume your server responds with the updated post data
         const updatedPost = await response.json();
         setPosts((prevPosts) =>
           prevPosts.map((post) =>
@@ -45,7 +123,6 @@ const Forum = () => {
     }
   };
 
-
   const tagOptions = [
     'General',
     'Discussions',
@@ -56,8 +133,6 @@ const Forum = () => {
     // Add more tags as needed
   ];
 
-  const [selectedTags, setSelectedTags] = useState([]);
-
   const handleTagChange = (tag) => {
     // Toggle the selected state of the tag
     if (selectedTags.includes(tag)) {
@@ -66,70 +141,21 @@ const Forum = () => {
       setSelectedTags((prevSelected) => [...prevSelected, tag]);
     }
   };
-
-  const samplePosts = [
-    {
-      title: 'Lost My Gains!',
-      author: 'GymNoob',
-      tags: ['Fitness', 'Gym'],
-      likes: 16,
-      dislikes: 2,
-      comments: 4,
-    },
-    {
-      title: 'The Great Protein Shake Debate',
-      author: 'ShakeLover',
-      tags: ['Protein', 'Humor'],
-      likes: 32,
-      dislikes: 1,
-      comments: 7,
-    },
-    {
-      title: 'How to Avoid Eye Contact at the Gym',
-      author: 'GymHider',
-      tags: ['Gym', 'Etiquette'],
-      likes: 19,
-      dislikes: 0,
-      comments: 3,
-    },
-    {
-      title: 'Cardio vs. Lifting',
-      author: 'FitFreak',
-      tags: ['Cardio', 'Lifting'],
-      likes: 45,
-      dislikes: 4,
-      comments: 12,
-    },
-    {
-      title: 'Disappearing Gym Socks',
-      author: 'SockDetective',
-      tags: ['Funny', 'Gym'],
-      likes: 22,
-      dislikes: 3,
-      comments: 5,
-    },
-    {
-      title: 'Muscles: How Many Are Too Many?',
-      author: 'MuscleMania',
-      tags: ['Muscles', 'Humor'],
-      likes: 38,
-      dislikes: 2,
-      comments: 8,
-    },
-    {
-      title: 'Protein Powder: A Love Story',
-      author: 'ProteinAddict',
-      tags: ['Protein', 'Love'],
-      likes: 33,
-      dislikes: 2,
-      comments: 7,
-    },
-  ];
   
-  const handleNewPostSubmit = () => {
+  const handleNewPostSubmit = async () => {
     // Perform validation
-    
-    // Add the new post to the main database
+    const response = await fetch("/api/forums", {
+      method: "POST",
+      body: JSON.stringify({
+        postTitle: postTitle,
+        postDescription: postDescription,
+        filterIds: selectedTags,
+      }),
+    });
+
+    if (response.ok) {
+      console.log("success added a post");
+    }
 
     // Close the popup
     handleClosePopup();
@@ -141,8 +167,6 @@ const Forum = () => {
     // Close the popup
     handleClosePopup();
   };
-
-  const [posts, setPosts] = useState(samplePosts);
   return (
     <div className="forum-container">
       {showNewPostPopup && <div className="blur-overlay"></div>}
@@ -218,11 +242,11 @@ const Forum = () => {
             </div>
             <div className="input-field">
               <label>Title</label>
-              <input type="text" placeholder="Enter title" />
+              <input type="text" placeholder="Enter title" onChange={(e) => setPostTitle(e.target.value)} />
             </div>
             <div className="input-field">
               <label>Description</label>
-              <textarea placeholder="Enter description"></textarea>
+              <textarea placeholder="Enter description" onChange={(e) => setPostDescription(e.target.value)} ></textarea>
             </div>
             <div className="input-field">
                 <div className='tag-text'>
