9459f8ce8360d2b0a14ba623c1d6b87e0c6fc37b
jlau <justinlau226@gmail.com>
2023-11-29 18:25:03 -0800

    MVPs done

--- src/app/listing/[id]/page.js
+++ src/app/listing/[id]/page.js
@@ -1,171 +1,175 @@
-"use client"
-import { useRouter } from 'next/navigation'; // Make sure to import useRouter
-import { Box, Typography, Button } from "@mui/material";
-import LocationOnIcon from "@mui/icons-material/LocationOn";
-import React, { useEffect, useState } from "react";
-import { doc, getDoc } from "firebase/firestore";
+"use client";
+import { useParams } from "next/navigation";
+import { useEffect, useState } from "react";
+import {
+  CircularProgress,
+  Grid,
+  Typography,
+  Paper,
+  Box,
+  Button,
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  DialogActions,
+  TextField,
+  Breadcrumbs,
+  Link,
+  Card,
+  CardMedia,
+  CardContent,
+  IconButton
+} from "@mui/material";
+import SwipeableViews from "react-swipeable-views";
+import { virtualize } from "react-swipeable-views-utils";
+import ShareIcon from "@mui/icons-material/Share";
 import { db } from "../../../../firebase/firebaseConfig";
+import { doc, getDoc } from "firebase/firestore";
+import LocationOnIcon from "@mui/icons-material/LocationOn";
+import GoogleMapReact from 'google-map-react';
 
-function ListingDetails() {
-  const [newListing, setListing] = useState(null);
-  const [user, setUser] = useState(null);
-  const router = useRouter(); // Use the useRouter hook to access the router object
-  const { id } = router.query; // Destructure the id from the router.query
+// Virtualized SwipeableViews for better performance
+const VirtualizeSwipeableViews = virtualize(SwipeableViews);
+
+const ListingPage = () => {
+  const { id } = useParams();
+  const [listing, setListing] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState("");
+
+  // State for selected image
+  const [selectedImage, setSelectedImage] = useState(0);
+
+  // ... existing loading and error handling
+
+  // Function to handle image selection
+  const handleImageSelect = (index) => {
+    setSelectedImage(index);
+  };
+
+  // Google Maps settings
+  // const mapOptions = {
+  //   center: {
+  //     lat: listing.location.lat, // Assuming 'location' contains 'lat' and 'lng'
+  //     lng: listing.location.lng,
+  //   },
+  //   zoom: 11,
+  // };
 
   useEffect(() => {
-    const fetchListingAndUser = async () => {
-      if (!router.isReady) return; // Make sure the router is ready and the id is available
+    if (id) {
+      const fetchListing = async () => {
+        setLoading(true);
+        try {
+          const docRef = doc(db, "listings", id);
+          const docSnap = await getDoc(docRef);
 
-      // Fetch listing details
-      const listingRef = doc(db, "listings", id);
-      const listingSnap = await getDoc(listingRef);
-
-      if (listingSnap.exists()) {
-        const listingData = listingSnap.data();
-        setListing(listingData);
-
-        // Fetch user details
-        const userRef = doc(db, "users", listingData.sellerId);
-        const userSnap = await getDoc(userRef);
-
-        if (userSnap.exists()) {
-          setUser(userSnap.data());
-        } else {
-          console.error("No such user!");
+          if (docSnap.exists()) {
+            setListing({ id: docSnap.id, ...docSnap.data() });
+          } else {
+            setError("Listing not found");
+          }
+        } catch (err) {
+          setError("An error occurred while fetching the listing");
+          console.error(err);
+        } finally {
+          setLoading(false);
         }
-      } else {
-        console.error("No such listing!");
-      }
-    };
+      };
 
-    fetchListingAndUser();
-  }, [router.isReady, id]); // Depend on the id to re-run the effect when it changes
+      fetchListing();
+    }
+  }, [id]);
 
-  if (!newListing || !user) {
-    return <div>Loading...</div>; // Show loading state while data is being fetched
+  // Function to render slide
+  const renderSlide = ({ index, key }) => {
+    return (
+      <img
+        key={key}
+        src={listing.images[index]}
+        alt={`${listing.title} - image ${index + 1}`}
+        style={{ width: "100%", height: "auto" }}
+      />
+    );
+  };
+
+  if (loading) {
+    return (
+      <Grid
+        container
+        justifyContent="center"
+        alignItems="center"
+        style={{ height: "100vh" }}
+      >
+        <CircularProgress />
+      </Grid>
+    );
+  }
+
+  if (error) {
+    return (
+      <Typography variant="h6" color="error" textAlign="center">
+        {error}
+      </Typography>
+    );
+  }
+
+  if (!listing) {
+    return null;
   }
 
   return (
-    <Box
-      sx={{
-        width: "100%",
-        borderRadius: "8px",
-        overflow: "hidden",
-        cursor: "pointer",
-      }}
-    >
-      <Box align="center" justifyContent="space-between">
-        <Button
-          sx={{ margin: "40px", backgroundColor: "grey", color: "white" }}
-          variant="contained"
-          component={Link}
-          href={"/"}
-        >
-          Go Back
-        </Button>
-        <Button
-          sx={{ margin: "40px", backgroundColor: "4FB18C", color: "white" }}
-          variant="contained"
-        >
-          Favorite
-        </Button>
-      </Box>
-      <Box sx={{ padding: 2 }}>
-        <Typography
-          variant="h4"
-          align="center"
-          sx={{ fontWeight: "bold", marginBottom: 1 }}
-        >
-          {newListing.title}
+    <Box sx={{ padding: 10 }}>
+      <Breadcrumbs aria-label="breadcrumb">
+        <Link underline="hover" color="inherit" href="/">
+          Home
+        </Link>
+        <Typography color="text.primary">{listing.title}</Typography>
+      </Breadcrumbs>
+
+      <Paper elevation={3} sx={{ mt: 2, p: 2 }}>
+        <Typography variant="h4" gutterBottom>
+          {listing.title}
         </Typography>
-        <Box
-          align="center"
-          sx={{
-            boxShadow: "0px 0px 15px rgba(0, 0, 0, 0)",
-            borderRadius: "8px 8px 0px 0px",
-          }}
-        >
-          <img
-            src={newListing.images}
-            alt="Product"
-            width="60%"
-            height="60%"
-            style={{
-              borderRadius: "8px",
-              boxShadow: "0px 4px 4px 0px rgba(0, 0, 0, 0.25)",
-            }}
-          />
-        </Box>
 
-        <Box display="flex" alignItems="center" justifyContent="center">
-          <Box
-            margin="40px"
-            sx={{
-              boxShadow: "0px 0px 15px rgba(0, 0, 0, 0)",
-              borderRadius: "8px 8px 0px 0px",
-              marginRight: 5,
+        {listing.images && listing.images.length > 0 && (
+          // This is used for efficient rendering of large sets of images.
+          <VirtualizeSwipeableViews slideRenderer={renderSlide} />
+        )}
+
+        <Typography variant="body1" gutterBottom>
+          {listing.description}
+        </Typography>
+        <Typography variant="h6" gutterBottom>
+          Price: ${listing.price}
+        </Typography>
+        <Typography variant="body2" color="textSecondary" gutterBottom>
+          Location: {listing.location}
+        </Typography>
+
+        <Box sx={{ display: "flex", justifyContent: "space-between", mt: 2 }}>
+          <Button
+            variant="contained"
+            startIcon={<LocationOnIcon />}
+            onClick={() => {
+              /* Implement Map View */
             }}
           >
-            <Typography
-              variant="h6"
-              sx={{ fontWeight: "bold", marginBottom: 1 }}
-            >
-              Price: ${newListing.price}
-            </Typography>
-            <Typography variant="h7">
-              Description: {newListing.description}
-            </Typography>
-          </Box>
-
-          <Box
-            margin="40px"
-            sx={{
-              boxShadow: "0px 0px 15px rgba(0, 0, 0, 0)",
-              borderRadius: "8px 8px 0px 0px",
-              marginLeft: 85,
+            View on Map
+          </Button>
+          <Button
+            variant="outlined"
+            startIcon={<ShareIcon />}
+            onClick={() => {
+              /* Implement Share Functionality */
             }}
           >
-            <Typography variant="h6" sx={{ fontWeight: "bold", marginLeft: 1 }}>
-              Contact Info
-            </Typography>
-
-            <Box
-              sx={{ display: "flex", flexDirection: "column", marginTop: 1 }}
-            >
-              <Typography
-                variant="h7"
-                sx={{ fontWeight: "bold", marginLeft: 1 }}
-              >
-                Name: {user.name}
-              </Typography>
-              <Typography
-                variant="h7"
-                sx={{ fontWeight: "bold", marginLeft: 1 }}
-              >
-                Phone: {user.phone}
-              </Typography>
-              <Typography
-                variant="h7"
-                sx={{ fontWeight: "bold", marginLeft: 1 }}
-              >
-                Email: {user.email}
-              </Typography>
-            </Box>
-            <Box sx={{ display: "flex", alignItems: "center", marginTop: 1 }}>
-              <LocationOnIcon fontSize="small" color="primary" />
-              <Typography
-                variant="caption"
-                color="textSecondary"
-                sx={{ marginLeft: 1 }}
-              >
-                {user.location}
-              </Typography>
-            </Box>
-          </Box>
+            Share
+          </Button>
         </Box>
-      </Box>
+      </Paper>
     </Box>
   );
-}
+};
 
-export default ListingDetails;
+export default ListingPage;
